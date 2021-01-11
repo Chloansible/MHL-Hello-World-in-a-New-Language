8d4e325670c61c9d4415c9f11dbc008f208136af
Chloansible <56802540+Chloansible@users.noreply.github.com>
2021-01-11 15:19:02 +0000

    Created Hello World in C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MHL Local Hack Day: Build!!");
+            Console.WriteLine("I'm Chloe :)");
+            Console.WriteLine("This is my first solo hackathon! We'll see how it goes!");
+            
+        }
+    }
+}
